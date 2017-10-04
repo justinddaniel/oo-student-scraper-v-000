6e7886161e3543e0864134adaf2d87994f523c62
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 15:58:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,12 @@ class Scraper
 
   def self.scrape_index_page(index_url = "http://67.205.182.198:48121/fixtures/student-site/")
     html = open(index_url)
-    
+    doc = Nokogiri::HTML(html)
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-

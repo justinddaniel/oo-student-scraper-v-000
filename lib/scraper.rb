9e31cdf59bd534483d6fca5a694f33053cafd17b
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 17:04:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,13 @@ class Scraper
   def self.scrape_index_page(index_url = "http://67.205.182.198:48121/fixtures/student-site/")
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+    snames = doc.css("h4.student-name")
+    namesarray = []
+    snames.each do |a|
+      namesarray << a.text
+    end
     binding.pry
+    
 
   end
 

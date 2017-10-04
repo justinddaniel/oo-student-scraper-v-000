85a5596050c2a86c6ebd5d869c754011b18d4783
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 22:13:22 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,7 +38,6 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    binding.pry
     # attributes needed: twitter url, linkedin url, github url, blog url, profile quote, and bio.
     shash = {}
     shash[:twitter] = doc.css(".social-icon-container a").first.attributes.first[1].value

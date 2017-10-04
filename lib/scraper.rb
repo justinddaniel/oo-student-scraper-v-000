c1dfbe38277cb1955a9d26612368dcd7a7a526d6
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 18:46:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
     ssites = doc.css(".student-card a")
     ssitesstring = ssites.to_s
     ssitesstring.scan(/students.(\w|-){1,100}.html/)
-    
+
     # namesarray, locationsarray, and sitesarray should all be arrays of equal
     #length. Now they can be combined into an array of hashes, where each array
     #index is a hash of :name => namesarray[i], :location => locationsarray[i], etc.
